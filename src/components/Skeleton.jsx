a57f2a4acbb6f2e685a5860b2abbc823157e0837
Aaron Gabriel <aarongabriel147@gmail.com>
2022-06-17 13:32:08 -0700

    adding skeleton loading and removed all PX and replaced with REM

---
+++ src/components/Skeleton.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export default function Skeleton() {
+    return (
+        <>
+            <div className="skeleton-container">
+                <div className="skeleton-author-conatiner">
+                    <div className="skeleton-author"></div>
+                    <div className="skeleton-author"></div>
+                </div>
+                <div className="skeleton-quote"></div>
+                <div className="skeleton-btn-container">
+                    <div className="skeleton-buttons"></div>
+                    <div className="skeleton-buttons"></div>
+                </div>
+            </div>
+        </>
+    )
+}
